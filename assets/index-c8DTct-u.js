0e4b5427028cd88d3202f806bf25c45b886f4a67
kongbai312 <86761381+kongbai312@users.noreply.github.com>
2024-02-23 15:28:50 +0000

    Deploying to release from @ kongbai312/vue3_3dpark@04005944ed629154d7529bd2182b4be558c2b125 🚀

---
+++ assets/index-c8DTct-u.js
@@ -0,0 +1,1 @@
+import o from"./index-DRWNvXul.js";import{d as n,u as t,o as r,c as a,a as s,b as c}from"./index-B0_vHM7p.js";const d=n({__name:"index",setup(m){const e=t();return r(()=>{e.getToken()}),(u,p)=>(c(),a("main",null,[s(o)]))}});export{d as default};
