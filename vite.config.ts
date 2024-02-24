0a9fa5200b45e827051b4167e54bead42a2af43a
Blank_空白 <312781556@qq.com>
2024-02-24 12:58:17 +0800

    配置打包环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import AutoImport from 'unplugin-auto-import/vite';
+import AutoImport from 'unplugin-auto-import/vite'
 import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    ReactivityTransform(),
-    AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ['vue'],
-      dts:"src/auto-import.d.ts"
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    port:8080,
-    open:true
-  }
-})
+// @ts-ignore
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    plugins: [
+      vue(),
+      ReactivityTransform(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue'],
+        dts: 'src/auto-import.d.ts'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 8080,
+      open: true
+    },
+    //github部署
+    base: env.MODE === 'production' ? '/vue3_3dpark/' : './'
+  })
+}
