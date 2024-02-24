a9b0230c7ee33503275a1e4bc98b0173377710d1
Blank_空白 <312781556@qq.com>
2024-02-24 15:15:09 +0800

    修改渲染逻辑

--- src/views/BigScreenView/model3d.vue
+++ src/views/BigScreenView/model3d.vue
@@ -12,6 +12,8 @@
 import { Application } from '@splinetool/runtime';
 // 导入loading组件
 import LoadingComponent from '@/components/LoadingComponent.vue';
+import { useUserStore } from '@/stores';
+
 // 初始化3d模型
 const ref3d = ref<HTMLCanvasElement>()
 //模型基本地址
@@ -22,17 +24,40 @@ const showLoading = ref(false)
 const init3dModel = async() => {
     // 开启loading 
     showLoading.value = true
-    // // 实例化解析器实例
-    // let spline = new Application(ref3d.value as HTMLCanvasElement)
-    // // 拉取模型
-    // await spline.load(`${publisPath}/scene.splinecode`)
-    // console.log('3D模型加载并渲染完毕')
-    // //关闭加载条
-    // showLoading.value = false
+    // 实例化解析器实例
+    let spline = new Application(ref3d.value as HTMLCanvasElement)
+    // 拉取模型
+    await spline.load(`${publisPath}/scene.splinecode`)
+    console.log('3D模型加载并渲染完毕')
+    //关闭加载条
+    showLoading.value = false
 }
+
+// //引入store
+// const useStore = useUserStore()
+// //token
+// let token = computed(() => {
+//   return useStore.token || ''
+// })
+// //监视本地token,有token才发送请求获取信息
+// const stopWatch = watch(() => token.value,async() => {
+//   //token存在
+//   if(token.value !== ''){
+//     //获取信息
+//     await init3dModel()
+//     //停止监听
+//     stopWatch()
+//   }
+// },{
+//   immediate:true
+// })
+
+
 // dom节点渲染完毕再加载
 onMounted(() => {
-    init3dModel()
+    nextTick(() => {
+        init3dModel()
+    })
 })
 
 </script>
